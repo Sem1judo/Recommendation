53753ef3eae4caacbf6f4dac46e4816f72a2ed3b
Semkov Stanislav <34599107+Sem1judo@users.noreply.github.com>
2022-12-04 11:16:34 +0200

    Update RecommendationsDataProvider.kt

--- app/src/main/java/com/example/cityengrecommendation/data/RecommendationsDataProvider.kt
+++ app/src/main/java/com/example/cityengrecommendation/data/RecommendationsDataProvider.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.cityengrecommendation.data
 
@@ -34,23 +19,53 @@ object RecommendationsDataProvider {
                 subtitleResourceId = R.string.english_subtitle,
                 description = R.string.english_subtitle,
                 imageResourceId = R.drawable.ic_baseball_square,
+                categoryId = 1
+
             ),
             Recommendation(
                 id = 2,
+                titleResourceId = R.string.english,
+                subtitleResourceId = R.string.english_subtitle,
+                description = R.string.english_subtitle,
+                imageResourceId = R.drawable.ic_badminton_square,
+                categoryId = 1
+
+            ),
+            Recommendation(
+                id = 3,
                 titleResourceId = R.string.german,
                 subtitleResourceId = R.string.german_subtitle,
                 description = R.string.english_subtitle,
-
                 imageResourceId = R.drawable.ic_badminton_square,
+                categoryId = 2
 
-                ),
+            ),
             Recommendation(
-                id = 3,
+                id = 4,
+                titleResourceId = R.string.german,
+                subtitleResourceId = R.string.german_subtitle,
+                description = R.string.english_subtitle,
+                imageResourceId = R.drawable.ic_basketball_square,
+                categoryId = 2
+
+            ),
+            Recommendation(
+                id = 5,
                 titleResourceId = R.string.spanish,
                 subtitleResourceId = R.string.spanish_subtitle,
                 description = R.string.english_subtitle,
-                imageResourceId = R.drawable.ic_basketball_square
+                imageResourceId = R.drawable.ic_basketball_square,
+                categoryId = 3
+            ),
+            Recommendation(
+                id = 6,
+                titleResourceId = R.string.spanish,
+                subtitleResourceId = R.string.spanish_subtitle,
+                description = R.string.english_subtitle,
+                imageResourceId = R.drawable.ic_badminton_square,
+                categoryId = 3
             )
+
         )
 
     }
